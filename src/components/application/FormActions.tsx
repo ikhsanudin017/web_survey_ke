f8c328d1fb6fe12823670ea9f7dc8c2a2dc86872
ikhsanudin017 <ikhsankuliah017@gmail.com>
2025-08-27 10:44:59 +0700

    fix: Add props definition to FormActions component

--- src/components/application/FormActions.tsx
+++ src/components/application/FormActions.tsx
@@ -1,8 +1,25 @@
-export default function FormActions() {
+import React from 'react';
+
+interface FormActionsProps {
+  currentStep: number;
+  totalSteps: number;
+  onNext: () => Promise<void>;
+  onPrevious: () => void;
+  isSubmitting: boolean;
+}
+
+export default function FormActions({
+  currentStep,
+  totalSteps,
+  onNext,
+  onPrevious,
+  isSubmitting,
+}: FormActionsProps) {
   return (
     <div>
       <h2>Form Actions</h2>
       {/* Add your component logic here */}
+      {/* You can use currentStep, totalSteps, onNext, onPrevious, isSubmitting here */}
     </div>
   );
 }
