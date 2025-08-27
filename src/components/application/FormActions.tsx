b8be55e4e7b21ee538aa7822f33d0a103b2f8fd0
ikhsanudin017 <ikhsankuliah017@gmail.com>
2025-08-27 10:48:42 +0700

    fix: Update onSave prop type in FormActions to accept data

--- src/components/application/FormActions.tsx
+++ src/components/application/FormActions.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 interface FormActionsProps {
   currentStep: number;
   totalSteps: number;
-  onNext: () => Promise<void>;
+  onNext?: () => Promise<void>;
+  onSave?: (data: any) => Promise<void>;
   onPrevious: () => void;
   isSubmitting: boolean;
 }
